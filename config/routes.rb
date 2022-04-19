8b6ca317cb461fac161a7b12508192116f92cfcc
hagaryota <ryota_haga@mediado.jp>
2022-04-19 14:09:55 +0900

    1410

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
-
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   
   if Rails.env.development?
     require "sidekiq/web"
